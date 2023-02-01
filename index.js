91bdab3f55ca461a553120e2daf8c8d79af5ad69
kvksatish <kvksatish98@gmail.com>
2023-02-01 21:18:16 +0530

    new

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express")
+const cors = require("cors")
 require("dotenv").config()
 const { connection } = require("./Config/db.js")
 const { dataController } = require("./Routes/data.routes.js")
 
-
+app.use(cors())
 
 const app = express()
 app.use(express.json())
