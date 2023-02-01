4ba2784cdeda9ea92030df2eec4ab727e7689e5c
kvksatish <kvksatish98@gmail.com>
2023-02-01 20:38:14 +0530

    n

---
+++ Config/db.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose")
+require("dotenv").config()
+const connection = mongoose.connect(process.env.MONGO_URL)
+//console.log(process.env.MONGO_URL)
+
+module.exports = { connection }
--- mvc/Models/DataModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const datascheme = new mongoose.Schema({
-    data: Array
-})
-const DataModel = mongoose.model("data", datascheme)
-
-
-module.exports = { DataModel }
